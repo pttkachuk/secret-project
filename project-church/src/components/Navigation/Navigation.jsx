c8ea9e8e54957ea4f4fe81ca09d91a441b7c4312
pttkachuk <petrotkachuk6@gmail.com>
2024-01-31 16:05:53 +0100

    working on header.

--- project-church/src/components/Navigation/Navigation.jsx
+++ project-church/src/components/Navigation/Navigation.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+//import { LiaBibleSolid } from "react-icons/lia";
+import { Cont, Home, StyledNavContainer } from "./NavigationStyled";
 
 const Navigation = () => {
-  return <div>Navigation</div>;
+  return (
+    <StyledNavContainer>
+      <Cont>
+        <Home>Home</Home>
+      </Cont>
+    </StyledNavContainer>
+  );
 };
 
 export default Navigation;
--- project-church/src/components/Navigation/NavigationStyled.js
+++ project-church/src/components/Navigation/NavigationStyled.js
@@ -1,2 +1,22 @@
 import styled from 'styled-components';
 
+export const StyledNavContainer = styled.div`
+  //width: 100%;
+  //max-width: 1440px;
+  display: flex;
+  //outline: 1px solid black;
+`;
+
+export const Cont = styled.div`
+width: 100%;
+background-color: #808000;
+border-radius: 5px;
+padding: 10px;
+box-shadow: rgba(149, 157, 165, 0.2) 0px 8px 24px;
+`
+
+export const Home = styled.h2`
+font-family: Kanit;
+font-weight: 500;
+color: #fff;
+`
