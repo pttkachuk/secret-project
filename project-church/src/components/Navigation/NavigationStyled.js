7dc535f04daecb6a1b75195eddd491cac6ccb3df
pttkachuk <petrotkachuk6@gmail.com>
2024-02-01 16:27:21 +0100

    upd nav styles

--- project-church/src/components/Navigation/NavigationStyled.js
+++ project-church/src/components/Navigation/NavigationStyled.js
@@ -4,19 +4,46 @@ export const StyledNavContainer = styled.div`
   //width: 100%;
   //max-width: 1440px;
   display: flex;
-  //outline: 1px solid black;
-`;
+  padding: 10px;
+  `;
 
 export const Cont = styled.div`
 width: 100%;
-background-color: #808000;
-border-radius: 5px;
-padding: 10px;
-box-shadow: rgba(149, 157, 165, 0.2) 0px 8px 24px;
+outline: 1px solid black;
+//padding: 10px;
+//background-color: #808000;
+//border-radius: 5px;
+//box-shadow: rgba(149, 157, 165, 0.2) 0px 8px 24px;
 `
 
 export const Home = styled.h2`
-font-family: Kanit;
-font-weight: 500;
-color: #fff;
+//margin-left: 35px ;
+font-family: Montserrat;
+font-weight: 600;
+color: black;
 `
+
+export const NavList = styled.ul`
+margin-left: auto;
+display: flex;
+align-items: center;
+text-align: center;
+gap: 15px;
+
+`;
+
+export const NavItem = styled.li`
+//padding: 5px;
+//outline: 1px solid red;
+`;
+
+export const NavLink = styled.a`
+font-family: Montserrat;
+font-weight: 400;
+color: black;
+text-decoration: none;
+transition: 0.3s;
+&:hover{
+  color: #6cb4ee
+}
+`;
