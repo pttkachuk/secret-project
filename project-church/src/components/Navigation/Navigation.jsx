2759b96949ebbff1d68253009fae85c0fb7fba56
pttkachuk <petrotkachuk6@gmail.com>
2024-02-01 16:27:34 +0100

    working on navigation

--- project-church/src/components/Navigation/Navigation.jsx
+++ project-church/src/components/Navigation/Navigation.jsx
@@ -1,14 +1,37 @@
 import React from "react";
 //import { LiaBibleSolid } from "react-icons/lia";
-import { Cont, Home, StyledNavContainer } from "./NavigationStyled";
+import {
+  Cont,
+  Home,
+  NavItem,
+  NavLink,
+  NavList,
+  StyledNavContainer,
+} from "./NavigationStyled";
 
 const Navigation = () => {
   return (
-    <StyledNavContainer>
+    <nav>
       <Cont>
-        <Home>Home</Home>
+        <StyledNavContainer>
+          <Home>Home</Home>
+          <NavList>
+            <NavItem>
+              <NavLink href="#">Who we are</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="#">Our service</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="#">How to reach us</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="#">Where we are</NavLink>
+            </NavItem>
+          </NavList>
+        </StyledNavContainer>
       </Cont>
-    </StyledNavContainer>
+    </nav>
   );
 };
 
