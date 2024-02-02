dc8a7f51412e183000b187509c229dfecfb055fe
pttkachuk <petrotkachuk6@gmail.com>
2024-02-02 16:49:23 +0100

    add hero.jsx

---
+++ project-church/src/components/Hero/Hero.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import {
+  Container,
+  DivC,
+  HeroTitle,
+  ShapeBlob,
+  ShapeBlobFive,
+  ShapeBlobFour,
+  ShapeBlobOne,
+  ShapeBlobSix,
+  ShapeBlobThree,
+  ShapeBlobTwo,
+} from "./HeroStyled";
+
+const Hero = () => {
+  return (
+    <Container>
+      <DivC>
+        <ShapeBlob></ShapeBlob>
+        <ShapeBlobOne></ShapeBlobOne>
+        <ShapeBlobTwo></ShapeBlobTwo>
+        <ShapeBlobThree></ShapeBlobThree>
+        <ShapeBlobFour></ShapeBlobFour>
+        <ShapeBlobFive></ShapeBlobFive>
+        <ShapeBlobSix></ShapeBlobSix>
+      </DivC>
+      <HeroTitle>
+        Chiesa Evangelica
+        <br />
+        Bergamo
+      </HeroTitle>
+    </Container>
+  );
+};
+
+export default Hero;
