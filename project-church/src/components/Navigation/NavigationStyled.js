ebcfc2c7c2a453dce8c666f222eb8419824bf6c9
pttkachuk <petrotkachuk6@gmail.com>
2024-02-02 16:49:42 +0100

    upd navigation styles

--- project-church/src/components/Navigation/NavigationStyled.js
+++ project-church/src/components/Navigation/NavigationStyled.js
@@ -7,13 +7,13 @@ export const StyledNavContainer = styled.div`
 
 export const Cont = styled.div`
 width: 100%;
-//outline: 1px solid black;
+//outline: 1px solid white;
 `
 
 export const LogoDiv = styled.button`
 display: flex;
 background-color: transparent;
-//outline: 1px solid black;
+color: #fff;
 transition: 0.3s;
 border: none;
 cursor: pointer;
@@ -31,7 +31,7 @@ margin-left: 10px;
 font-family: Montserrat;
 font-weight: 600;
 transition: 0.3s;
-color: black;
+color: #fff;
 `
 
 export const NavList = styled.ul`
@@ -49,7 +49,7 @@ export const NavItem = styled.li`
 export const NavLink = styled.a`
 font-family: Montserrat;
 font-weight: 400;
-color: black;
+color: #fff;
 text-decoration: none;
 transition: 0.3s;
 &:hover{
