039c28a4b14892dd1aa759b30158cbf057f1ecbf
pttkachuk <petrotkachuk6@gmail.com>
2024-02-06 17:00:03 +0100

    upd. clean navigation component

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -15,36 +15,32 @@ const Navigation = () => {
     console.log("clicked btn");
   };
   return (
-    <nav>
-      <Cont>
-        <StyledNavContainer>
-          <LogoDiv onClick={handleClick}>
-            <PiChurchLight size={26} />
-            <Home>Parola di Fede</Home>
-          </LogoDiv>
-          <NavList>
-            <NavItem>
-              <NavLink href="#">Chi siamo</NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink href="#">Culti</NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink href="#">Dove siamo</NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink href="#">Contattaci</NavLink>
-            </NavItem>
-          </NavList>
-          {/* <select>
-            <option>ita</option>
-            <option>ukr</option>
-            <option>rus</option>
-            <option>eng</option>
-          </select> */}
-        </StyledNavContainer>
-      </Cont>
-    </nav>
+    <header>
+      <nav>
+        <Cont>
+          <StyledNavContainer>
+            <LogoDiv onClick={handleClick}>
+              <PiChurchLight size={26} />
+              <Home>Parola di Fede</Home>
+            </LogoDiv>
+            <NavList>
+              <NavItem>
+                <NavLink href="#">Chi siamo</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="#">Culti</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="#">Dove siamo</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="#">Contattaci</NavLink>
+              </NavItem>
+            </NavList>
+          </StyledNavContainer>
+        </Cont>
+      </nav>
+    </header>
   );
 };
 
