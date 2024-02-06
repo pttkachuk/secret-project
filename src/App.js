cf5720bedee6d8d318bfdd8af215f5a46824416a
pttkachuk <petrotkachuk6@gmail.com>
2024-02-06 14:04:50 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,11 +5,8 @@ import Navigation from './components/Navigation/Navigation';
 function App() {
   return (
     <div className='App'>
-
       <Navigation />
       <Hero />
-
-
     </div>
   );
 }
