190eb02838fb924133f9ab552fd2cc15e3ffbadf
pttkachuk <petrotkachuk6@gmail.com>
2024-02-08 15:24:10 +0100

    Create MapPage.jsx

---
+++ src/pages/MapPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import MapSection from "../components/Map/MapSection";
+
+const MapPage = () => {
+  return (
+    <div>
+      <MapSection />
+    </div>
+  );
+};
+
+export default MapPage;
