e2ec9d8d3134f94c606f221efd2d6fd7157dda1e
pttkachuk <petrotkachuk6@gmail.com>
2024-02-08 15:52:39 +0100

    add animatepresance

--- src/App.js
+++ src/App.js
@@ -1,29 +1,33 @@
 import { Suspense } from 'react';
-import { Navigate, Route, Routes } from 'react-router-dom';
+import { Navigate, Route, Routes, useLocation } from 'react-router-dom';
 import Footer from './components/Footer/Footer';
 import Navigation from './components/Navigation/Navigation';
 import Loader from './components/Loader';
+import { AnimatePresence } from "framer-motion";
 import { lazy } from 'react';
 
-const Home = lazy(() => import('./pages/HomePage'));
+const Home = lazy(() => import('./pages/HomePage/HomePage'));
 const Location = lazy(() => import('./pages/MapPage'));
 
 function App() {
+
+  const location = useLocation();
   return (
     <div>
       <Navigation />
-      <main>
+      <AnimatePresence>
         <Suspense fallback={<Loader />}>
-          <Routes>
+          <Routes location={location} key={location.pathname}>
             <Route path='/' element={<Home />} />
             <Route path='/location' element={<Location />} />
             <Route path='*' element={<Navigate to="/" replace />} />
           </Routes>
         </Suspense>
-      </main>
-      <footer>
-        <Footer />
-      </footer>
+        <footer>
+          <Footer />
+        </footer>
+      </AnimatePresence>
+
     </div>
   );
 }
