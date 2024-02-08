8e1439dd7e7ee01bb68288d01c68a3d6ad5dd985
pttkachuk <petrotkachuk6@gmail.com>
2024-02-08 15:24:17 +0100

    Create MapSectionStyled.js

---
+++ src/components/Map/MapSectionStyled.js
@@ -0,0 +1,26 @@
+import styled from 'styled-components';
+
+export const StyledMapSection = styled.section`
+box-sizing: border-box;
+display: flex;
+justify-content: center;
+`;
+
+export const StyledMapWrapper = styled.div`
+  box-sizing: border-box;
+  width: 100%;
+  max-width: 1440px;
+  height: 350px;
+  padding: 20px;
+//outline: 1px solid red;
+`;
+
+export const GoogleMap = styled.div`
+margin-left: auto;
+  width: 100%;
+  max-width: 600px;
+`;
+
+export const GoogleMapIframe = styled.iframe`
+
+`;
