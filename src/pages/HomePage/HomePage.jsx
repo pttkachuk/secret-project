dd202e7ee5bd611807fd90c214617acb6d2b7082
pttkachuk <petrotkachuk6@gmail.com>
2024-02-08 15:53:13 +0100

    add folder homePage & add motion.main

---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Hero from "../../components/Hero/Hero";
+import { motion } from "framer-motion";
+
+const HomePage = () => {
+  return (
+    <motion.main
+      initial={{ y: -20, opacity: 0 }}
+      animate={{ y: 0, opacity: 1 }}
+      transition={{ duration: 2, ease: [0.6, -0.05, 0.01, 0.99] }}
+    >
+      <Hero />
+    </motion.main>
+  );
+};
+
+export default HomePage;
