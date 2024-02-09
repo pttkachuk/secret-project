7bc081ab43fb8f4b4dabf040586d1e95d9b7a002
pttkachuk <petrotkachuk6@gmail.com>
2024-02-09 16:47:04 +0100

    Update ContactPage.jsx

--- src/pages/ContactPage/ContactPage.jsx
+++ src/pages/ContactPage/ContactPage.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { motion } from "framer-motion";
+import MailForm from "../../components/MailForm/MailForm";
 
 const ContactPage = () => {
   return (
@@ -8,7 +9,7 @@ const ContactPage = () => {
       animate={{ y: 0, opacity: 1 }}
       transition={{ duration: 1, ease: [0.6, -0.05, 0.01, 0.99] }}
     >
-      ContactPage
+      <MailForm />
     </motion.main>
   );
 };
