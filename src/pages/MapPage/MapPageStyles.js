8f6aad097f2deff72aa3d0d86101266ae307b290
pttkachuk <petrotkachuk6@gmail.com>
2024-02-09 12:43:36 +0100

    Create MapPageStyles.js

---
+++ src/pages/MapPage/MapPageStyles.js
@@ -0,0 +1,30 @@
+import styled from 'styled-components';
+
+export const MapPageTitle = styled.h2`
+    font-family: Montserrat;
+    font-weight: 600;
+    font-size: 28px;
+    line-height: 1.2;
+    //text-align: center;
+    padding: 20px 0 40px 0;
+`;
+
+export const MapPageDesc = styled.p`
+    margin: 0;
+    margin-bottom: 40px ;
+    padding: 0;
+    font-family: Montserrat;
+    font-weight: 400;
+    font-size: 20px;
+    line-height: 1.2;
+    //text-align: center;
+`;
+
+export const TextDiv = styled.div`
+height: 350px;
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items: center;
+text-align: center;
+`;
