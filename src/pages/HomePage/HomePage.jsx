920963fb304f2ee1af08d0f78af613a251619dfc
pttkachuk <petrotkachuk6@gmail.com>
2024-02-09 10:53:00 +0100

    upd styles

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -7,7 +7,7 @@ const HomePage = () => {
     <motion.main
       initial={{ y: -20, opacity: 0 }}
       animate={{ y: 0, opacity: 1 }}
-      transition={{ duration: 2, ease: [0.6, -0.05, 0.01, 0.99] }}
+      transition={{ duration: 1, ease: [0.6, -0.05, 0.01, 0.99] }}
     >
       <Hero />
     </motion.main>
--- src/pages/MapPage/MapPage.jsx
+++ src/pages/MapPage/MapPage.jsx
@@ -7,7 +7,7 @@ const MapPage = () => {
     <motion.main
       initial={{ y: -20, opacity: 0 }}
       animate={{ y: 0, opacity: 1 }}
-      transition={{ duration: 2, ease: [0.6, -0.05, 0.01, 0.99] }}
+      transition={{ duration: 1, ease: [0.6, -0.05, 0.01, 0.99] }}
     >
       <MapSection />
     </motion.main>
