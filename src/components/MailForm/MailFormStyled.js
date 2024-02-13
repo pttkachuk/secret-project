d123580c1267e0d57dda0684a4b118f2f3b2b69a
pttkachuk <petrotkachuk6@gmail.com>
2024-02-13 12:09:10 +0100

    Update MailFormStyled.js

--- src/components/MailForm/MailFormStyled.js
+++ src/components/MailForm/MailFormStyled.js
@@ -4,6 +4,7 @@ export const ContactsSection = styled.section`
 box-sizing: border-box;
 display: flex;
 justify-content: center;
+flex-wrap: wrap;
 `;
 
 export const ContactsDiv = styled.div`
@@ -28,7 +29,7 @@ max-width: 350px;
 export const FormDiv = styled.div`
 height: 100%;
 width: 100%;
-max-width: 550px;
+max-width: 450px;
 display: flex;
 flex-direction: column;
 align-items: center;
@@ -51,6 +52,29 @@ font-family: Montserrat;
 font-size: 16px;
 font-weight: 400;
 color: #fff;
+transition: 0.3s;
+position: relative;
+
+&::before{
+    content: '';
+    display: block;
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    inset: 0 0 0 0;
+    background: #4EAEFF;
+    z-index: -1;
+    transform: scaleX(0);
+    transform-origin: bottom right;
+    transition: transform .3s ease;
+}
+
+&:hover::before{
+    transform: scaleX(1);
+    transform-origin: bottom left;
+}
 `;
 
 /////////////////////////////////////////////////////////////
@@ -100,7 +124,7 @@ top: 20px;
   }
   padding-bottom: 6px;  
   font-weight: 400;
-  border-width: 2px;
+  border-width: 1px;
   border-image: linear-gradient(to right, #4EAEFF,#FDDA00);
   border-image-slice: 1;
 }
@@ -123,6 +147,7 @@ export const StyledTextArea = styled.textarea`
 resize: none;
 background: transparent;
 border: 0;
+outline: 0;
 border-bottom: 1px solid #d3d3d3;
 color: #fff;
 margin-bottom: 15px;
@@ -134,7 +159,7 @@ transition: 0.2s;
 }
 
 &:focus{
-border-width: 2px;
+border-width: 1px;
 border-image: linear-gradient(to right, #4EAEFF,#FDDA00);
 border-image-slice: 1;
 }
@@ -153,4 +178,79 @@ cursor: pointer;
 &:hover{
     color: #FDDA00;
 }
+`;
+
+/////////////////////////////////////////////////////////////
+
+export const PhoneDiv = styled.div`
+    box-sizing: border-box;
+    width: 100%;
+    max-width: 1440px;
+    padding: 20px;
+    //outline: 1px solid red;
+    height: 450px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 15%;
+`;
+
+export const PhoneDivTitle = styled.h2`
+font-family: Montserrat;
+font-size: 28px;
+font-weight: 700;
+color: #fff;
+`;
+
+export const PhoneTitleDiv = styled.div`
+//outline: 1px solid red;
+max-width: 350px;
+text-align: center;
+`;
+
+export const StyledPhoneLink = styled.a`
+text-decoration: none;
+font-family: Montserrat;
+font-size: 14px;
+font-weight: 400;
+color: #fff;
+transition: 0.3s;
+position: relative;
+
+&::before{
+    content: '';
+    display: block;
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    inset: 0 0 0 0;
+    background: #4EAEFF;
+    z-index: -1;
+    transform: scaleX(0);
+    transform-origin: bottom right;
+    transition: transform .3s ease;
+}
+
+&:hover::before{
+    transform: scaleX(1);
+    transform-origin: bottom left;
+}
+`;
+
+export const StyledPhoneTitle = styled.p`
+margin: 0;
+padding: 0;
+font-family: Montserrat;
+font-size: 18px;
+font-weight: 400;
+color: #fff;
+margin-bottom: 5px;
+`;
+
+export const StyledPhoneItem = styled.li`
+//outline: 1px solid red;
+margin-bottom: 10px;
+padding: 10px;
 `;
