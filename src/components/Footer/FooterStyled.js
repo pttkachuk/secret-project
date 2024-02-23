fd25762a6e762c14f9c3eb98a138fed67e2a0839
pttkachuk <petrotkachuk6@gmail.com>
2024-02-23 11:19:55 +0100

    upd hover effect at footer

--- src/components/Footer/FooterStyled.js
+++ src/components/Footer/FooterStyled.js
@@ -30,10 +30,6 @@ font-family: Montserrat;
 color: #d3d3d3;
 font-weight: 300;
 font-size: 14px;
-
-& :hover{
-    color: #FDDA00;
-}
 `;
 
 export const IconsContainer = styled.div`
@@ -84,7 +80,29 @@ cursor: pointer;
 `;
 
 export const LinkToContacts = styled(NavLink)`
-color: #d3d3d3;
+color: #fff;
 text-decoration:none;
-transition: all 0.3s ease-in-out;
+transition: 0.3s;
+position: relative;
+
+&::before{
+    content: '';
+    display: block;
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    inset: 0 0 0 0;
+    background: #0085FF;
+    z-index: -1;
+    transform: scaleX(0);
+    transform-origin: bottom right;
+    transition: transform .3s ease;
+}
+
+&:hover::before{
+    transform: scaleX(1);
+    transform-origin: bottom left;
+}
 `;
