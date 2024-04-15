b84a70114d9e61d8c426a44c4fdcf04eba5748d3
pttkachuk <petrotkachuk6@gmail.com>
2024-04-15 15:42:25 +0200

    upd

--- src/pages/MapPage/MapPage.jsx
+++ src/pages/MapPage/MapPage.jsx
@@ -9,7 +9,7 @@ const MapPage = () => {
     <motion.main
       initial={{ y: -20, opacity: 0 }}
       animate={{ y: 0, opacity: 1 }}
-      transition={{ duration: 1, ease: [0.6, -0.05, 0.01, 0.99] }}
+      transition={{ duration: 2, ease: [0.6, -0.05, 0.01, 0.99] }}
     >
       <TextDiv>
         <MapPageTitle>Come trovarci</MapPageTitle>
