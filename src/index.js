c93d6e311a22ac745af9b39a3c2ba07bfac3d1e3
pttkachuk <petrotkachuk6@gmail.com>
2024-09-23 19:05:22 +0200

    add i18n to mail project file

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import './helpers/i18n';
 import reportWebVitals from './helpers/reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 
