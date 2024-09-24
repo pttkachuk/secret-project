778416f4ecc8cefefa40400eb888d84fa1190b7c
pttkachuk <petrotkachuk6@gmail.com>
2024-09-24 21:29:30 +0200

    add new translation

---
+++ src/helpers/languages.js
@@ -0,0 +1,67 @@
+export const languages = {
+    en: {
+        translation: {
+            "logo": "Word of Faith",
+            "who we are": "Who we are",
+            "worship": "Worship Meetings",
+            "location": "Location",
+            "contacts": "Contacts",
+            "hero": "Evangelical Church",
+            "hero1": "Word Of Faith",
+            "footer": "Jesus loves you.",
+            "footer1": "Remember what He has done for you.",
+            "social": "Our socials",
+            "copyright": "© 2024. Word Of Faith. Bergamo.",
+            "contact us": "Contact us",
+        }
+    },
+    it: {
+        translation: {
+            "logo": "Parola di Fede",
+            "who we are": "Chi siamo",
+            "worship": "Riunioni di Culto",
+            "location": "Dove siamo",
+            "contacts": "Contatti",
+            "hero": "Chiesa Evangelica",
+            "hero1": "Parola Di Fede",
+            "footer": "Gesu ti ama.",
+            "footer1": "Ricordati cosa Lui ha fatto per te.",
+            "social": "Nostri social",
+            "copyright": "© 2024. Parola di Fede. Bergamo.",
+            "contact us": "Contattaci",
+        }
+    },
+    ru: {
+        translation: {
+            "logo": "Слово Веры",
+            "who we are": "О нас",
+            "worship": "Богослужения",
+            "location": "Адрес",
+            "contacts": "Контакты",
+            "hero": "Евангельская Церковь",
+            "hero1": "Слово Веры",
+            "footer": "Иисус любит тебя.",
+            "footer1": "Помни, что Он сделал для тебя.",
+            "social": "Наши соцсети",
+            "copyright": "© 2024. Слово Веры. Бергамо.",
+            "contact us": "Cвязаться с нами",
+        }
+    },
+    ua: {
+        translation: {
+            "logo": "Слово Віри",
+            "who we are": "Про нас",
+            "worship": "Богослужіння",
+            "location": "Адреса",
+            "contacts": "Контакти",
+            "hero": "Євангельська Церква",
+            "hero1": "Слово Віри",
+            "footer": "Ісус любить тебе.",
+            "footer1": "Пам'ятай, що Він зробив для тебе.",
+            "social": "Наші соцмережі",
+            "copyright": "© 2024. Слово Віри. Бергамо.",
+            "contact us": "Зв'язатися з нами",
+        }
+    },
+    // Aggiungi altre lingue se necessario
+};
