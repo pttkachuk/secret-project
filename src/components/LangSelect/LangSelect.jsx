82d5557e3f500627d20f9bdedd530ab4869677bf
pttkachuk <petrotkachuk6@gmail.com>
2024-09-25 20:32:15 +0200

    add react select

--- src/components/LangSelect/LangSelect.jsx
+++ src/components/LangSelect/LangSelect.jsx
@@ -1,22 +1,29 @@
 import React from "react";
+import Select from "react-select";
 import { useTranslation } from "react-i18next";
+import { selectStyles } from "./StyledLangSelect";
 
 const LangSelect = () => {
   const { i18n } = useTranslation();
 
-  const changeLanguage = (event) => {
-    i18n.changeLanguage(event.target.value); // Cambia la lingua in base al valore selezionato
+  const changeLanguage = (current) => {
+    i18n.changeLanguage(current.value); // Cambia la lingua in base al valore selezionato
   };
 
+  const langOptions = [
+    { value: "ua", label: "🇺🇦" },
+    { value: "ru", label: "🇷🇺" },
+    { value: "it", label: "🇮🇹" },
+    { value: "en", label: "🇬🇧" },
+  ];
+
   return (
-    <div>
-      <select onChange={changeLanguage} defaultValue={i18n.lng}>
-        <option value="ua">Українська</option>
-        <option value="ru">Руский</option>
-        <option value="it">Italiano</option>
-        <option value="en">English</option>
-      </select>
-    </div>
+    <Select
+      options={langOptions}
+      onChange={changeLanguage}
+      styles={selectStyles}
+      defaultValue={langOptions[0]}
+    />
   );
 };
 
